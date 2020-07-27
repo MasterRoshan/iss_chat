c1d50da72d4723c127df40968f6c4f0d3a0194c9
MasterRoshan <t22thejohnson@gmail.com>
2020-07-26 22:52:59 -0400

    initial chat implementation

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {
+  Widget,
+  toggleWidget,
+  addUserMessage,
+  addResponseMessage
+} from "react-chat-widget";
+import "react-chat-widget/lib/styles.css";
+import "./App.css";
 
 function App() {
+  const handleNewUserMessage = newMessage => {
+    console.log(`New message incoming! ${newMessage}`);
+    fetch(
+      `https://iss-dialog.uk.r.appspot.com/?text=${encodeURIComponent(
+        newMessage
+      )}`
+    )
+      .then(response => response.json())
+      .then(data => addResponseMessage(data.text));
+  };
+
+  toggleWidget();
+  addResponseMessage(
+    'Hello! I can tell you a few things about the international space station. Try asking: "Where is the ISS?" or "When is the next pass over 48.8584, 2.2945?"'
+  );
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Widget
+        handleNewUserMessage={handleNewUserMessage}
+        fullScreenMode={true}
+        title={"International Space Station Chat Bot"}
+        subtitle={"Fun with ML and the ISS API"}
+      />
     </div>
   );
 }
