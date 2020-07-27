b421fd93dd9dc5f39b4eb9596c5f5a026cab1642
MasterRoshan <t22thejohnson@gmail.com>
2020-07-26 23:02:55 -0400

    Updates

---
+++ precache-manifest.1e35a75cd7fbee5a4c7ccc4d04fb11eb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f27bc0d655d19da9f95bcd09ba9478f",
+    "url": "/iss_chat/index.html"
+  },
+  {
+    "revision": "cb72d1b18932518f2ccc",
+    "url": "/iss_chat/static/css/2.29c4d543.chunk.css"
+  },
+  {
+    "revision": "c6b72cec8f741b7cc533",
+    "url": "/iss_chat/static/css/main.1b51ef2e.chunk.css"
+  },
+  {
+    "revision": "cb72d1b18932518f2ccc",
+    "url": "/iss_chat/static/js/2.0263082c.chunk.js"
+  },
+  {
+    "revision": "7a0c0758d9bfdc25a40edd58cae2b6db",
+    "url": "/iss_chat/static/js/2.0263082c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c6b72cec8f741b7cc533",
+    "url": "/iss_chat/static/js/main.69cb3bff.chunk.js"
+  },
+  {
+    "revision": "ba94da9ce4ef0e152033",
+    "url": "/iss_chat/static/js/runtime-main.48addf9b.js"
+  }
+]);
